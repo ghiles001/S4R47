11d4ef40830c6f214887127587cf6fe8a35097e1
ghiles001 <34516928+ghiles001@users.noreply.github.com>
2020-02-06 20:38:40 +0100

    Update google.py

--- google.py
+++ google.py
@@ -1,6 +1,32 @@
 from googlesearch import search
-query = input("Search for (site.com): ")
-a = int(input("Nbr of searches(1000): "))
-for i in range(a):
-    for t in search(query, tld = 'com', lang = 'en', num = 10, start = 0, stop = None, pause = 2.0 ):
-        print(t)
+
+query = str(input("Search for : "))
+a = int(input("how many times: "))
+mod = str(input("Mode of search (verbos/silent): "))
+
+if mod == "verbos" or mod == "VERBOS" or mod == "Verbos":
+
+    for i in range(a):
+
+        for t in search(query, tld = 'com', lang = 'en', num = 10, start = 0, stop = None, pause = 2.0 ):
+            print(t)
+
+elif mod == "silent" or mod == "SILENT" or mod == "Silent":
+
+    gen = input("Genrate file as: ")
+    f = open(gen, "w+")
+
+    def w_file(t):
+        global  f
+        f.write(t+"%d\r\n")
+        res = "Results of Serch saved as "+ gen
+
+
+    for i in range(a):
+        k = 0
+        for t in search(query, tld = 'com', lang = 'en', num = 10, start = 0, stop = None, pause = 2.0 ):
+            k += 1
+            w_file(t)
+        print("{} Result".format(k))
+else:
+    print("EROR in mode of search !!!")
